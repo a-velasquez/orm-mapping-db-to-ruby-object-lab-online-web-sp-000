c9816e01af6d6a838f26472cb5e0015316ca585e
Alex Velasquez <jtinnesco@gmail.com>
2019-12-07 01:41:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,6 +67,14 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+
+    SQL
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
