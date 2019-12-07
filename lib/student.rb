4fb6c415b3b3cd6aeeac95d6861982036ca47f58
Alex Velasquez <jtinnesco@gmail.com>
2019-12-07 01:39:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,8 +60,8 @@ class Student
       SELECT *
       FROM students
       WHERE grade=10
-      LIMIT ?;"
-    SQL  
+      LIMIT ?;
+    SQL
       DB[:conn].execute(sql, num).collect do |row|
       self.new_from_db(row)
     end
