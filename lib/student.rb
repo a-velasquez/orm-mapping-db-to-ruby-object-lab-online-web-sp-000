7b80680d6370bbca6bd1318355cb9ec07468ec82
Alex Velasquez <jtinnesco@gmail.com>
2019-12-07 01:36:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,12 +56,10 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(num)
-    sql = <<-SQL
-      SELECT * FROM students
-      WHERE grade = 10 LIMIT ?
-    SQL
+    sql = "SELECT * FROM students WHERE grade=10 ORDER BY students.id LIMIT ?;"
     DB[:conn].execute(sql, num)
   end
+
 
 
   def save
